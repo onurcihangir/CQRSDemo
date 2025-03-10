c23b825888e35ecd40450f42b24fce5aeaec3ccb
Onur Cihangir <o.cihangir@baylanwatermeters.com>
2025-03-10 11:48:06 +0300

    gateway swagger

--- MeterSystem.APIGateway/Program.cs
+++ MeterSystem.APIGateway/Program.cs
@@ -4,14 +4,26 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Configuration.AddJsonFile("ocelot.json");
-builder.Services.AddOcelot().AddCacheManager(x =>
-{
-    x.WithDictionaryHandle();
-});
+builder.Services
+    .AddSwaggerForOcelot(builder.Configuration)
+    .AddOcelot(builder.Configuration)
+    .AddCacheManager(x =>
+    {
+        x.WithDictionaryHandle();
+    });
 
 var app = builder.Build();
 
-await app.UseOcelot();
+await app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+}).UseOcelot();
+
+app.MapControllers();
 
 app.Run();
